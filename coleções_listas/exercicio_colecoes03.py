291d906aa55eedf4af9538e870ed8447002bcb75
octopsarju <gustavoarju2323@gmail.com>
2026-04-09 19:42:22 -0300

    Adicionando  exercicio 03

--- coleções_listas/exercicio_colecoes03.py
+++ coleções_listas/exercicio_colecoes03.py
@@ -3,15 +3,26 @@
 #idade: 20
 #curso: engenharia
 
-aluno = {
-
-    'Nome':'Maria',
-    'Idade': '20',
-    'Curso':'Engenharia'
-}
+aluno = {'Nome':'Maria','Idade':'20','Curso':'Engenharia'}
 print(aluno)
 
 #Agora realizando solicitações do exercicio.
 
-aluno.setdefault('Nota', 9.5)
+aluno['Nota'] = '9,5'
+print()
 print(aluno)
+
+aluno['Idade'] = '21'
+print()
+print(aluno)
+
+aluno.pop('Curso')
+print()
+print(aluno)
+
+aluno['Cursos'] = ['Engenharia de Software' , 'Engenharia de Computação', 'Ciência da Computação']
+print()
+print(aluno)
+
+print()
+print('Fim')
