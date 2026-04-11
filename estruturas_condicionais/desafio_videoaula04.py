2cbb4b52d779869b6d45fbe77491396e1f933d44
octopsarju <gustavoarju2323@gmail.com>
2026-04-10 22:01:25 -0300

    Mini mudanças.

--- estruturas_condicionais/desafio_videoaula04.py
+++ estruturas_condicionais/desafio_videoaula04.py
@@ -1,6 +1,6 @@
 atletas = str(input("Digite seu nome: "))
 idade = int(input("Digite sua idade: "))
-permissao_medica = False
+permissao_medica = True
 condicionamento_fisico = True
 
 if idade >= 18 and idade <= 35 and permissao_medica == True and condicionamento_fisico == True or idade >= 18 and idade <= 35 and permissao_medica == True and condicionamento_fisico == False:
