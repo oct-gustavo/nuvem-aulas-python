0e5a20d3cf9662feeb7e8507015eaaf79aeb94c8
octopsarju <gustavoarju2323@gmail.com>
2026-04-11 11:42:10 -0300

    Commitando estudos em loops.

---
+++ loops_python/loops.py
@@ -0,0 +1,12 @@
+#for x in range (1, 101):
+#    print(f'Isso é um loop! Estou na {x}º vez!' )
+#    print('Se inscreve no canal!\n')
+
+#variavel soma
+soma = 0 
+#loop fazendo somar 0 + 1 até 10 
+for x in range('1, 11'):
+    #linha abaixo acrescenta 1 a soma
+    soma += 1
+    #printando a variavel soma adicionando um simulando +1 de 1 até 10
+print(f'A soma total é {soma}')
---
+++ loops_python/loops_string.py
@@ -0,0 +1,17 @@
+#Loops com Listas
+names = ['Maicon', 'Gustavo', 'Bia', 'Gabriel']
+
+for n in names:
+    print(n)
+
+#loops com tuplas
+names = ('Maicon', 'Gustavo', 'Bia', 'Gabriel')
+
+for n in names:
+    print(n)
+
+#loops com variavel
+names = 'Maicon'
+
+for lyrics in names:
+    print(lyrics)
