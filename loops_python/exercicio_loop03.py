34310f4069eb42a974184ab2d6873a81f481b901
octopsarju <gustavoarju2323@gmail.com>
2026-04-12 21:44:54 -0300

    Alterações no exercicio 03 (Não finalizado)

--- loops_python/exercicio_loop03.py
+++ loops_python/exercicio_loop03.py
@@ -1,3 +1,11 @@
 #Exercicio 03 - Escreva um programa que conta quantas vogais tem na string que o usuario digitar.
 
-frase = str(input('Digite uma frase: '))
+frase = str(input('Digite uma frase: ')) 
+vogais = 'AEIOU' or 'aeiou'
+for v in vogais:
+    contador_vogais = 1
+    contador_vogais += 1
+    print(f'Tem exatas {contador_vogais} vogais na frase.')
+
+
+    #NÃO FINALIZADO.
